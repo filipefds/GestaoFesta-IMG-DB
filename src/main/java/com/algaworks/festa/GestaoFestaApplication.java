1b807109ab06777f0a97a8a506f833cfadd82243
Filipe Freitas <contato.filipefreitas@gmail.com>
2017-12-15 17:27:37 -0200

    20171215

--- src/main/java/com/algaworks/festa/GestaoFestaApplication.java
+++ src/main/java/com/algaworks/festa/GestaoFestaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestaoFestaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestaoFestaApplication.class, args);
+		SpringApplication.run(GestaoFestaApplication.class, args);	
 	}
 	
 }
